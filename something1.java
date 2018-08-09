a232973832ace000bf2de124533ec4317b388aa4
rajatSinha303 <rajat.sinha@tno.nl>
2018-08-09 11:58:18 +0200

    testing git char length for 1 line

--- something1.java
+++ something1.java
@@ -1,2 +1,2 @@
 String name = "Rajat"
-String thisIsIncorrect = "Are you sure?";
+String thisIsIncorrect = "Are you sure?";String thisIsIncorrect = "Are you sure?";String thisIsIncorrect = "Are you sure?";String thisIsIncorrect = "Are you sure?";String thisIsIncorrect = "Are you sure?";String thisIsIncorrect = "Are you sure?";String thisIsIncorrect = "Are you sure?";String thisIsIncorrect = "Are you sure?";String thisIsIncorrect = "Are you sure?";
